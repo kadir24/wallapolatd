ae6bfed733cd7800959bf4370d7117f0618bec68
kadir24 <kadir.siker1907@gmail.com>
2017-03-04 08:44:39 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,604 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>
+Kadir Polat</title><link rel="stylesheet" type="text/css" href="" media="all,handheld"/><link rel="shortcut icon" href="">
+<style>
+html {
+
+  background: url(http://www.tumblrtemalari.com/wp-content/uploads/1.gif) no-repeat center center fixed ;
+
+  -webkit-background-size: cover;
+
+  -moz-background-size: cover;
+
+  -o-background-size: cover;
+
+  background-size: cover;
+ 
+ font-family: monospace;
+}
+
+.button {
+    
+    border: none;
+    border-radius: 30px;
+    color: white;
+    width: 135px;
+    height: 33px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 25px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+	padding: 2px ;
+}
+
+.button1 {
+    
+	background-color: #18a689;
+	
+    color: white;
+	
+    border: 1px solid rgba(35, 34, 35, 0.39);
+}
+
+.button1:hover {
+    
+	background-color: rgba(35, 34, 35, 0.39);
+    
+	color: black;
+    
+	border: 1px solid #319db5;
+}
+
+.token {
+
+	background-color: #18a689;
+
+	font-size: 22px ;	
+	
+	font-family: Candice;
+
+	color: white;
+	
+	width: 60% ;
+}	
+
+.heading {
+	
+	background-color: #18a689;
+
+	font-size: 40px ;	
+
+	color: white;
+	
+	width: 20% ;
+	
+	font-family: Candice;
+}
+.heading1 {
+	
+	background-color: #18a689;
+
+	font-size: 30px ;	
+
+	color: white;
+	
+	width: 70% ;
+	
+	font-family: Candice;
+}
+
+.inp {
+
+    border: none;
+
+    color: white;
+
+    width: 700px;
+
+    height: 35px;
+
+    text-align: left;
+
+    text-decoration: none;
+
+    display: inline-block;
+
+    font-size: 16px;
+
+    margin: 4px 2px;
+
+    -webkit-transition-duration: 0.4s; /* Safari */
+
+    transition-duration: 0.4s;
+}
+
+.text {
+	
+	background-color: #f1f1f1;
+    
+	color: black;
+    
+	border: 1px solid white;
+    
+	border-radius: 30px;
+}
+
+.text:hover {
+	
+	background-color: rgba(35, 34, 35, 0.39);
+}
+
+.footer {
+	
+	background-color: #18a689;
+
+	font-size: 40px ;	
+
+	color: white;
+	
+	width: 27% ;
+	
+	font-family: Comic Sans MS;
+}
+
+.sarmadkhokhar {
+	
+	background-color: #18a689;
+	
+}
+
+</style><?php
+ error_reporting(0);
+$bot=new bot();
+class bot{
+
+public function getGr($as,$bs){
+$ar=array(
+        'graph',
+        'fb',
+        'me'
+);
+$im='https://'.implode('.',$ar);
+
+return $im.$as.$bs;
+}
+
+public function getUrl($mb,$tk,$uh=null){
+$ar=array(
+        'access_token' => $tk,
+);
+if($uh){
+$else=array_merge($ar,$uh);
+        }else{
+        $else=$ar;
+}
+foreach($else as $b => $c){
+        $cokis[]=$b.'='.$c;
+}
+$true='?'.implode('&',$cokis);
+$true=$this->getGr($mb,$true);
+$true=json_decode($this->
+one($true),true);
+if($true[data]){
+        return $true[data];
+}else{
+        return $true;}
+}
+
+private function one($url){
+$cx=curl_init();
+curl_setopt_array($cx,array(
+CURLOPT_URL => $url,
+CURLOPT_CONNECTTIMEOUT => 5,
+CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_USERAGENT => 'DESCRIPTION by MAHAL_KITA',
+));
+$ch=curl_exec($cx);
+        curl_close($cx);
+        return ($ch);
+}
+
+public function savEd($tk,$id,$a,$b,$o,$c,$z=null,$bb=null){
+if(!is_dir('cokis')){
+        mkdir('cokis');
+}
+if($bb){
+$blue=fopen('cokis/'.$id,'w');
+fwrite($blue,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c.'*'.$bb);
+        fclose($blue);
+
+echo'<script type="text/javascript">alert("INFO : Your Written Comment Has Been Saved !! Good Luck !! Ashir")</script>';
+}else{
+        if($z){
+if(file_exists('cokis/'.$id)){
+$file=file_get_contents('cokis/'.$id);
+$ex=explode('*',$file);
+$str=str_replace($ex[0],$tk,$file);
+$xs=fopen('cokis/'.$id,'w');
+        fwrite($xs,$str);
+        fclose($xs);
+}else{
+$str=$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c;
+$xs=fopen('cokis/'.$id,'w');
+        fwrite($xs,$str);
+        fclose($xs);
+}
+$_SESSION[key]=$tk.'_'.$id;
+}else{
+$file=file_get_contents('cokis/'.$id);
+$file=explode('*',$file);
+        if($file[5]){
+$up=fopen('cokis/'.$id,'w');
+fwrite($up,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c.'*'.$file[5]);
+        fclose($up);
+        }else{
+$up=fopen('cokis/'.$id,'w');
+fwrite($up,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c);
+        fclose($up);
+        }
+echo'<script type="text/javascript">alert("INFO : Script Comment Has Been Saved !! Good Luck !! Ashir")</script>';}}
+}
+
+public function lOgbot($d){
+        unlink('cokis/'.$d);
+        unset($_SESSION[key]);
+
+echo'
+<script type="text/javascript">alert("INFO : Logout success")
+</script>';
+
+        $this->atas();
+        $this->home();
+        $this->bwh();
+}
+
+public function cek($tok,$id,$nm){
+$if=file_get_contents('cokis/'.$id);
+$if=explode('*',$if);
+if(preg_match('/on/',$if[1])){
+        $satu='on';
+        $ak='Like tambah komen';
+}else{
+        $satu='off';
+        $ak='Like saja';
+}
+if(preg_match('/on/',$if[2])){
+        $dua='on';
+        $ik='Bot emo';
+}else{
+        $dua='off';
+        $ik='Bot manual';
+}
+if(preg_match('/on/',$if[3])){
+        $tiga='on';
+        $ek='Powered on';
+}else{
+        $tiga='off';
+        $ek='Powered off';
+}
+if(preg_match('/on/',$if[4])){
+        $empat='on';
+        $uk='Text via script';
+}else{
+        $empat='off';
+        $uk='Via text sendiri';
+}
+echo'
+<div id="bottom-content">
+<div id="navigation-menu">
+<h3><a name="navigation-name" class="no-link">PENGATURAN BOT by '.$nm.'</a></h3>
+<ul>
+<li>
+Welcome Back : <font color="Black">'.$nm.'</font></li>
+<li>
+<a href="http://m.facebook.com/'.$id.'"><img src="https://graph.facebook.com/'.$id.'/picture" style="width:50px; height:50px;" alt="'.$nm.'"/></a></li>
+<li>
+<form action="index.php" method="post"><input type="hidden" name="logout" value="'.$id.'">
+<input type="submit" value="Logout Bot"></form></li>
+<li>
+<form action="index.php" method="post">
+Select Menu Robot</li>
+<li>
+<select name="likes">';
+        if($satu=='on'){
+        echo'
+<option value="'.$satu.'">
+'.$ak.'
+</option>
+<option value="off">
+Like saja</option>
+</select>';
+        }else{
+        echo'
+<option value="'.$satu.'">
+'.$ak.'
+</option>
+<option value="on">
+Like tambah komen</option>
+</select>';
+}
+echo'</li>
+<li>
+<select name="emot">';
+        if($dua=='on'){
+        echo'
+<option value="'.$dua.'">
+'.$ik.'
+</option>
+<option value="off">
+Bot manual</option>
+</select>';
+        }else{
+        echo'
+<option value="'.$dua.'">
+'.$ik.'
+</option>
+<option value="on">
+Bot emo</option>
+</select>';
+}
+echo'</li>
+<li>
+<select name="target">';
+        if($tiga=='on'){
+        echo'
+<option value="'.$tiga.'">
+'.$ek.'
+</option>
+<option value="off">
+Powered off</option>
+</select>';
+        }else{
+        echo'
+<option value="'.$tiga.'">
+'.$ek.'
+</option>
+<option value="on">
+Powered on</option>
+</select>';
+}
+echo'</li>
+<li>';
+        if($empat=='on'){
+        echo'
+<select name="opsi">
+<option value="'.$empat.'">
+'.$uk.'
+</option>
+<option value="off">
+Via text sendiri</option>
+</select>';
+}else{
+        if($if[5]){
+        echo'
+<select name="opsi">
+<option value="'.$empat.'">
+'.$uk.'
+</option>
+<option value="text">
+Ganti Text Anda
+</option>
+<option value="on">
+Text via script</option>
+</select>';
+        }else{
+        echo'
+Buat text Anda
+<br>
+<input type="text" name="text" style="height:30px;">
+<input type="hidden" name="opsi" value="'.$empat.'">';}
+}
+echo'
+</li>
+</ul></div>
+
+<div id="top-content">
+<div id="search-form">
+<input type="submit" value="Başlat"></form>
+</div></div></div>';
+
+$this->membEr();
+}
+
+public function atas(){
+$hari=array(1=>
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+);
+
+$bulan=array(1=>
+"January",
+  "February",
+    "March",
+     "April",
+       "May",
+         "June",
+           "July",
+             "August",
+               "September",
+          "October",
+     "November",
+"Desember"
+);
+
+$hr=$hari[gmdate('N',time()+60*60*7)];
+$tgl=gmdate('j',time()+60*60*7);
+$bln=
+$bulan[gmdate('n',time()+60*60
+*7)];
+$thn=gmdate('Y',time()+60*60*7);
+$jam=gmdate('H',time()+60*60*7);
+
+echo'
+<div id="header"><center>
+<h1 class="heading">
+Kadir Polat
+</h1></center>
+<h2 class="description">
+</h2></div>';
+}
+
+public function home(){
+echo'
+<div id="content">
+<div class="post"><center>
+<div class="sarmadkhokhar">
+<img src="heading.png" width="55%"></img>
+<img src="heading.gif" width="50%"></img>
+
+</div></center>
+<div class="post-content">
+</div>
+<div class="post-meta2">
+</div></div></div>';
+}
+
+public function bwh(){
+echo'
+<div id="bottom-content">
+
+<div id="navigation-menu"><center>
+
+
+<div class="token"><h1></h1></center>
+
+
+</div>
+
+<ul>
+
+<center>
+
+<a href="https://developers.facebook.com/tools/debug/accesstoken/?app_id=41158896424" target="_blank" value="Token Al" class="button button1">Token Al</a>
+
+</center>
+ 
+ <br>
+
+ </center>
+
+</br><div class="heading1"><center><h2>
+
+
+
+</h2></center></div>
+
+<div id="top-content">
+<center>
+<div id="search-form">
+
+<form action="index.php" method="post">
+
+<input class="inp text " type="text" name="token"> 
+
+<input class="button button1" type="submit" value="Giriş">
+
+</form></div></center></div></div>';
+
+$this->membEr();
+}
+
+public function membEr(){
+        if(!is_dir('cokis')){
+        mkdir('cokis');
+}
+$up=opendir('cokis');
+while($use=readdir($up)){
+if($use != '.' && $use != '..'){
+        $user[]=$use;}
+        }
+
+echo'<center>
+<div class="footer">
+Aktif Bot <font color="red">'.count($user).'</font>
+</div>
+<audio src="song.mp3" autoplay>
+<embed src="/music/good_enough.mp3" width="180" height="90" hidden="true" />
+</audio>
+</center>';
+}
+
+public function toXen($h){
+header('Location: https://m.facebook.com/dialog/oauth?client_id='.$h.'&redirect_uri=https://www.facebook.com/connect/login_success.html&display=wap&scope=publish_actions%2Cuser_photos%2Cuser_friends%2Cfriends_photos%2Cuser_activities%2Cuser_likes%2Cuser_status%2Cuser_groups%2Cfriends_status%2Cpublish_stream%2Cread_stream%2Cread_requests%2Cstatus_update&response_type=token&fbconnect=1&from_login=1&refid=9');
+}
+
+
+}
+if(isset($_SESSION[key])){
+        $a=$_SESSION[key];
+        $ai=explode('_',$a);
+        $a=$ai[0];
+if($_POST[logout]){
+        $ax=$_POST[logout];
+        $bot->lOgbot($ax);
+}else{
+$b=$bot->getUrl('/me',$a,array(
+'fields' => 'id,name',
+));
+if($b[id]){
+if($_POST[likes]){
+        $as=$_POST[likes];
+        $bs=$_POST[emot];
+        $bx=$_POST[target];
+        $cs=$_POST[opsi];
+        $tx=$_POST[text];
+if($cs=='text'){
+        unlink('cokis/'.$b[id]);
+$bot->savEd($a,$b[id],$as,$bs,$bx,'off');
+        }else{
+        if($tx){
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs,'x',$tx);
+        }else{
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs);}}
+}
+        $bot->atas();
+        $bot->home();
+$bot->cek($a,$b[id],$b[name]);
+}else{
+echo '<script type="text/javascript">alert("INFO: Session Token Expired")</script>';
+        unset($_SESSION[key]);
+        unlink('cokis/'.$ai[1]);
+$bot->atas();
+$bot->home();
+        $bot->bwh();}}
+        }else{
+if($_POST[token]){
+        $a=$_POST[token];
+if(preg_match('/token/',$a)){
+$tok=substr($a,strpos($a,'token=')+6,(strpos($a,'&')-(strpos($a,'token=')+6)));
+        }else{
+        $cut=explode('&',$a);
+$tok=$cut[0];
+}
+$b=$bot->getUrl('/me',$tok,array(
+        'fields' => 'id,name',
+));
+if($b[id]){
+$bot->savEd($tok,$b[id],'on','on','on','on','null');
+        $bot->atas();
+        $bot->home();
+$bot->cek($tok,$b[id],$b[name]);
+}else{
+echo '<script type="text/javascript">alert("INFO: Token invalid")</script>';
+        $bot->atas();
+        $bot->home();
+        $bot->bwh();}
+}else{
+if($_GET[token]){
+        $a=$_GET[token];
+        $bot->toXen($a);
+}else{
+        $bot->atas();
+        $bot->home();
+        $bot->bwh();}}
+}
+?>
+
